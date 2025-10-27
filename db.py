df98b22a0e5c757b5c1b5cbea74e64adaf8a6c1a
iganya <matthewiganga@gmail.com>
2025-10-27 19:28:54 +0100

    Modified database setting

--- db.py
+++ db.py
@@ -3,19 +3,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-MYSQL_DB = os.getenv("MYSQL_DB")
+# MYSQL_USER = os.getenv("MYSQL_USER")
+# MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+# MYSQL_HOST = os.getenv("MYSQL_HOST")
+# MYSQL_PORT = os.getenv("MYSQL_PORT")
+# MYSQL_DB = os.getenv("MYSQL_DB")
 
 # DB_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
-DB_URL = f"mysql+pymysql://user:QiPHEWAwmLoTxdCcUNVXVxonSNbeKsBu@mysql.railway.internal:3306/railway"
+# DB_URL = f"mysql+pymysql://user:QiPHEWAwmLoTxdCcUNVXVxonSNbeKsBu@mysql.railway.internal:3306/railway"
 
-
-
+DB_URL = os.getenv('DATABASE_URL')
+print(DB_URL)
+if not DB_URL:
+    raise ValueError("DATABASE_URL is not set, check your envorment variables")
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
